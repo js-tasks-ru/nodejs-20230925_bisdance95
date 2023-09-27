a4a4a54cfd775ae52a1128e7ce9b95b5f1c40186
Бийсолтан Бийсолтанов <bijjsoltan.bijjsoltanov@vitech.team>
2023-09-28 00:19:35 +0300

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,9 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  }
+
+  throw new TypeError('Все переданные аргументы должны быть числами');
 }
 
 module.exports = sum;
